501b49ae0c53b537351f60431f36ac632d1d07d7
Evgenia Kiseleva <janekis@aol.com>
2014-03-03 20:59:59 -0600

    fixing .gitignore

--- app/controllers/site_controller.rb
+++
@@ -1,6 +0,0 @@
-class SiteController < ApplicationController
-  def index
-    # retrieve all Bookmarks ordered by descending creation timestamp
-    @bookmarks = Bookmark.order('created_at desc')
-  end
-end
